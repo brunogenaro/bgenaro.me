0640e2e642b982ed54d7f18e9194f0a1f0563de6
Matheus Corrêa <mcorreadesign@gmail.com>
2022-02-21 22:55:13 -0300

    feat: Podcasts responsiveness

--- src/components/PodcastsBanner/index.tsx
+++ src/components/PodcastsBanner/index.tsx
@@ -3,25 +3,25 @@ import SeeAll from '../SeeAll'
 
 const PodcastsBanner: React.FC = () => (
   <section className="my-10">
-    <h3 className="ml-5 mb-10 bg-gradient-to-br from-pink-500 to-purple-500 bg-clip-text text-7xl font-bold tracking-wide  text-transparent">
+    <h3 className="ml-5 mb-10 bg-gradient-to-br from-pink-500 to-purple-500 bg-clip-text text-2xl font-bold tracking-wide  text-transparent sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl">
       podcasts
     </h3>
-    <ul className="flex-colum">
+    <ul className="w-full">
       <PodcastsCard
         title="Dicas de como evoluir profissionalmente como Desenvolvedor Web"
         description="Bruno começou a carreira como desenvolvedor web em Piracicaba e hoje trabalha na Cisco em Orlando/FL. "
         path="/"
         date="14 novembro 2020"
       />
-      <div className="flex justify-end">
-        <PodcastsCard
-          title="Dicas de como evoluir profissionalmente como Desenvolvedor Web"
-          description="Bruno começou a carreira como desenvolvedor web em Piracicaba e hoje trabalha na Cisco em Orlando/FL. "
-          path="/"
-          date="14 novembro 2020"
-          mirrored
-        />
-      </div>
+      <PodcastsCard
+        title="Dicas de como evoluir profissionalmente como Desenvolvedor Web"
+        description="Bruno começou a carreira como desenvolvedor web em Piracicaba e hoje trabalha na Cisco em Orlando/FL. "
+        path="/"
+        date="14 novembro 2020"
+        mirrored
+        customStyles="mr-0"
+        alignment="justify-end"
+      />
     </ul>
     <SeeAll title="podcasts" path="/podcasts" />
   </section>
--- src/components/PodcastsCard/index.tsx
+++ src/components/PodcastsCard/index.tsx
@@ -1,6 +1,5 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import { TextContainer, Thumbnail } from './styles'
 
 interface iPodcastsCard {
   title: string
@@ -8,7 +7,8 @@ interface iPodcastsCard {
   path: string
   date: string
   mirrored?: boolean
-  className?: string
+  customStyles?: string
+  alignment?: string
 }
 
 export const PodcastsCard: React.FC<iPodcastsCard> = ({
@@ -17,84 +17,82 @@ export const PodcastsCard: React.FC<iPodcastsCard> = ({
   path,
   date,
   mirrored,
-  className,
+  customStyles,
+  alignment,
 }: iPodcastsCard) => (
-  <li className={`my-5 flex items-center ${className}`}>
+  <li className={`my-5 ${customStyles}`}>
     {!mirrored ? (
-      <>
-        <Thumbnail className="overflow-hidden rounded-3xl">
-          <Image
-            src="/assets/image/purple.jpg"
-            alt="talk image"
-            objectFit="cover"
-            width={350}
-            height={350}
-            layout="responsive"
-          />
-        </Thumbnail>
-        <TextContainer className="relative my-5 -ml-44 h-fit rounded-3xl bg-gray-800 p-10">
-          <Link href={path}>
-            <a href={path}>
-              <h4 className="mb-3 text-2xl font-bold tracking-wide text-gray-300">
-                {title}
-              </h4>
-              <p className="mb-5 text-lg tracking-wide text-gray-300">
-                {description}
-              </p>
-              <span className="text-gray-300">{date}</span>
-            </a>
-          </Link>
-        </TextContainer>
-        <div className="z-10 -ml-8 flex items-center justify-center rounded-xl bg-purple-500 p-4">
-          <Image
-            src="/assets/image/podcasts.svg"
-            alt="talk image"
-            width={32}
-            height={32}
-          />
-        </div>
-      </>
+      <Link href={path}>
+        <a href={path} className={`flex items-center ${alignment}`}>
+          <div className="h-80 w-80 overflow-hidden rounded-3xl">
+            <Image
+              src="/assets/image/purple.jpg"
+              alt="talk image"
+              objectFit="cover"
+              width={350}
+              height={350}
+              layout="responsive"
+            />
+          </div>
+          <div className="relative -ml-44 h-fit w-3/5 rounded-3xl bg-gray-800 p-10">
+            <h4 className="mb-3 text-2xl font-bold tracking-wide text-gray-300">
+              {title}
+            </h4>
+            <p className="mb-5 text-lg tracking-wide text-gray-300">
+              {description}
+            </p>
+            <span className="text-gray-300">{date}</span>
+          </div>
+          <div className="z-10 -ml-8 flex items-center justify-center rounded-xl bg-purple-500 p-4">
+            <Image
+              src="/assets/image/podcasts.svg"
+              alt="talk image"
+              width={32}
+              height={32}
+            />
+          </div>
+        </a>
+      </Link>
     ) : (
-      <>
-        <div className="z-20 flex items-center justify-center rounded-xl bg-purple-500 p-4">
-          <Image
-            src="/assets/image/podcasts.svg"
-            alt="talk image"
-            width={32}
-            height={32}
-          />
-        </div>
-        <TextContainer className="relative z-10 my-5 -ml-8  h-fit rounded-3xl bg-gray-800 p-10">
-          <Link href={path}>
-            <a href={path}>
-              <h4 className="mb-3 text-2xl font-bold tracking-wide text-gray-300">
-                {title}
-              </h4>
-              <p className="mb-5 text-lg tracking-wide text-gray-300">
-                {description}
-              </p>
-              <span className="text-gray-300">{date}</span>
-            </a>
-          </Link>
-        </TextContainer>
-        <Thumbnail className="-ml-44 overflow-hidden rounded-3xl">
-          <Image
-            src="/assets/image/purple.jpg"
-            alt="talk image"
-            objectFit="cover"
-            width={350}
-            height={350}
-            layout="responsive"
-          />
-        </Thumbnail>
-      </>
+      <Link href={path}>
+        <a href={path} className={`flex items-center ${alignment}`}>
+          <div className="z-20 flex items-center justify-center rounded-xl bg-purple-500 p-4">
+            <Image
+              src="/assets/image/podcasts.svg"
+              alt="talk image"
+              width={32}
+              height={32}
+            />
+          </div>
+          <div className="relative z-10  -ml-8 h-fit  w-3/5 rounded-3xl bg-gray-800 p-10">
+            <h4 className="mb-3 text-2xl font-bold tracking-wide text-gray-300">
+              {title}
+            </h4>
+            <p className="mb-5 text-lg tracking-wide text-gray-300">
+              {description}
+            </p>
+            <span className="text-gray-300">{date}</span>
+          </div>
+          <div className="-ml-44 h-80 w-80 overflow-hidden rounded-3xl">
+            <Image
+              src="/assets/image/purple.jpg"
+              alt="talk image"
+              objectFit="cover"
+              width={350}
+              height={350}
+              layout="responsive"
+            />
+          </div>
+        </a>
+      </Link>
     )}
   </li>
 )
 
 PodcastsCard.defaultProps = {
   mirrored: false,
-  className: '',
+  customStyles: '',
+  alignment: '',
 }
 
 export default PodcastsCard
