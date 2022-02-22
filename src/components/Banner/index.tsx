ac3c366ecfffeccba6c84ada3e882908212c9a9d
Matheus Corrêa <mcorreadesign@gmail.com>
2022-02-21 22:42:15 -0300

    feat: Banner responsiveness

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,18 +1,18 @@
 import Image from 'next/image'
 import TypeAnimation from 'react-type-animation'
-import ImageContainer, { ImageBorder, InsertionPointer } from './style'
+import ImageContainer, { ImageBorder, InsertionPointer } from './styles'
 
 const Banner: React.FC = () => (
-  <section className="flex items-center justify-center py-10">
-    <div className=" mr-50">
-      <span className="ml-1 text-3xl tracking-wide text-gray-300">
-        Hello, Im
+  <section className="flex items-center justify-between 2xl:px-20">
+    <div>
+      <span className="ml-1 text-sm tracking-wide text-gray-300 sm:text-base md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl">
+        Hello, I&apos;m
       </span>
-      <h1 className="text-8xl font-bold tracking-wide text-gray-300">
+      <h1 className="text-3xl font-bold tracking-wide text-gray-300 sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl 2xl:text-8xl">
         Bruno Genaro
       </h1>
       <div className="flex items-center">
-        <h2 className=" bg-gradient-to-br from-cyan-500 to-blue-500 bg-clip-text text-8xl font-bold tracking-wide text-transparent">
+        <h2 className=" bg-gradient-to-br from-cyan-500 to-blue-500 bg-clip-text text-3xl font-bold tracking-wide text-transparent sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl 2xl:text-8xl">
           <TypeAnimation
             cursor
             sequence={['speaker', 3000, 'developer', 3000, 'father', 3000]}
@@ -22,8 +22,8 @@ const Banner: React.FC = () => (
         <InsertionPointer className="bg-gradient-to-br from-cyan-500 to-blue-500" />
       </div>
     </div>
-    <ImageBorder className="ml-36 bg-gradient-to-br from-cyan-500 to-blue-500  shadow-lg shadow-cyan-500/50">
-      <ImageContainer>
+    <ImageBorder className="flex items-center justify-center rounded-full bg-gradient-to-br from-cyan-500 to-blue-500 shadow-lg shadow-cyan-500/50">
+      <ImageContainer className=" overflow-hidden rounded-full">
         <Image
           src="/assets/image/bruno.jpeg"
           alt="Bruno Genaro Picture"
---
+++ src/components/Banner/styles.ts
@@ -0,0 +1,110 @@
+import styled from 'styled-components'
+
+const ImageContainer = styled.div`
+  @media (min-width: 640px) {
+    width: 140px;
+    height: 140px;
+  }
+  @media (min-width: 768px) {
+    width: 210px;
+    height: 210px;
+  }
+
+  @media (min-width: 1024px) {
+    width: 240px;
+    height: 240px;
+  }
+
+  @media (min-width: 1280px) {
+    width: 280px;
+    height: 280px;
+  }
+
+  @media (min-width: 1536px) {
+    width: 300px;
+    height: 300px;
+  }
+
+  width: 350px;
+  height: 350px;
+`
+
+export const ImageBorder = styled.div`
+  @media (min-width: 640px) {
+    width: 160px;
+    height: 160px;
+  }
+  @media (min-width: 768px) {
+    width: 230px;
+    height: 230px;
+  }
+
+  @media (min-width: 1024px) {
+    width: 260px;
+    height: 260px;
+  }
+
+  @media (min-width: 1280px) {
+    width: 300px;
+    height: 300px;
+  }
+
+  @media (min-width: 1536px) {
+    width: 330px;
+    height: 330px;
+  }
+
+  width: 370px;
+  height: 370px;
+`
+
+export const InsertionPointer = styled.span`
+  @media (min-width: 640px) {
+    width: 5px;
+    height: 30px;
+  }
+  @media (min-width: 768px) {
+    width: 6px;
+    height: 40px;
+    margin-left: -5px;
+  }
+
+  @media (min-width: 1024px) {
+    width: 7px;
+    height: 50px;
+    margin-left: -10px;
+  }
+
+  @media (min-width: 1280px) {
+    width: 8px;
+    height: 60px;
+    margin-left: -15px;
+  }
+
+  @media (min-width: 1536px) {
+    width: 9px;
+    height: 75px;
+    margin-left: -20px;
+  }
+
+  width: 10px;
+  height: 80px;
+  margin-left: -25px;
+
+  @keyframes flickerAnimation {
+    0% {
+      opacity: 1;
+    }
+    50% {
+      opacity: 0;
+    }
+    100% {
+      opacity: 1;
+    }
+  }
+
+  opacity: 1;
+  animation: flickerAnimation 2s infinite;
+`
+
+export default ImageContainer
