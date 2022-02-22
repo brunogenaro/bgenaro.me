3be0b47c926bc6005768c4b62fd75dbfeb003483
Matheus Corrêa <mcorreadesign@gmail.com>
2022-02-21 22:58:22 -0300

    feat: Home responsiveness

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,7 @@ import ProjectsBanner from '../components/ProjectsBanner'
 import TalksBanner from '../components/TalksBanner'
 
 const Home: NextPage = () => (
-  <div className="px-52">
+  <div className="px-10 sm:px-16 md:px-24 lg:px-32 xl:px-40 2xl:px-52">
     <Banner />
     <TalksBanner />
     <PodcastsBanner />
