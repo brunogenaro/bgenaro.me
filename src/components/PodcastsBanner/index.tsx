10e93f696e827854bcb2da0ff3ca7ab349dfdaf1
Matheus Corrêa <mcorreadesign@gmail.com>
2022-02-23 17:39:45 -0300

    feat: PodcastCard

--- src/components/PodcastsBanner/index.tsx
+++ src/components/PodcastsBanner/index.tsx
@@ -6,22 +6,37 @@ const PodcastsBanner: React.FC = () => (
     <h3 className="ml-5 mb-10 bg-gradient-to-br from-pink-500 to-purple-500 bg-clip-text text-2xl font-bold tracking-wide  text-transparent sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl">
       podcasts
     </h3>
-    <ul className="w-full">
-      <PodcastsCard
-        title="Dicas de como evoluir profissionalmente como Desenvolvedor Web"
-        description="Bruno começou a carreira como desenvolvedor web em Piracicaba e hoje trabalha na Cisco em Orlando/FL. "
-        path="/"
-        date="14 novembro 2020"
-      />
-      <PodcastsCard
-        title="Dicas de como evoluir profissionalmente como Desenvolvedor Web"
-        description="Bruno começou a carreira como desenvolvedor web em Piracicaba e hoje trabalha na Cisco em Orlando/FL. "
-        path="/"
-        date="14 novembro 2020"
-        mirrored
-        customStyles="mr-0"
-        alignment="justify-end"
-      />
+    <ul>
+      <div className="flex pb-5">
+        <PodcastsCard
+          title="Dicas de como evoluir profissionalmente como Desenvolvedor Web"
+          description="Bruno começou a carreira como desenvolvedor web em Piracicaba e hoje trabalha na Cisco em Orlando/FL. "
+          path="/"
+          date="14 novembro 2020"
+        />
+        <PodcastsCard
+          title="Dicas de como evoluir profissionalmente como Desenvolvedor Web"
+          description="Bruno começou a carreira como desenvolvedor web em Piracicaba e hoje trabalha na Cisco em Orlando/FL. "
+          path="/"
+          date="14 novembro 2020"
+          customStyles="mr-0"
+        />
+      </div>
+      <div className="flex">
+        <PodcastsCard
+          title="Dicas de como evoluir profissionalmente como Desenvolvedor Web"
+          description="Bruno começou a carreira como desenvolvedor web em Piracicaba e hoje trabalha na Cisco em Orlando/FL. "
+          path="/"
+          date="14 novembro 2020"
+        />
+        <PodcastsCard
+          title="Dicas de como evoluir profissionalmente como Desenvolvedor Web"
+          description="Bruno começou a carreira como desenvolvedor web em Piracicaba e hoje trabalha na Cisco em Orlando/FL. "
+          path="/"
+          date="14 novembro 2020"
+          customStyles="mr-0"
+        />
+      </div>
     </ul>
     <SeeAll title="podcasts" path="/podcasts" />
   </section>
--- src/components/PodcastsCard/index.tsx
+++ src/components/PodcastsCard/index.tsx
@@ -6,7 +6,6 @@ interface iPodcastsCard {
   description: string
   path: string
   date: string
-  mirrored?: boolean
   customStyles?: string
   alignment?: string
 }
@@ -16,81 +15,47 @@ export const PodcastsCard: React.FC<iPodcastsCard> = ({
   description,
   path,
   date,
-  mirrored,
   customStyles,
   alignment,
 }: iPodcastsCard) => (
-  <li className={`my-5 ${customStyles}`}>
-    {!mirrored ? (
-      <Link href={path}>
-        <a href={path} className={`flex items-center ${alignment}`}>
-          <div className="h-80 w-80 overflow-hidden rounded-3xl">
+  <li className={`mx-1 h-fit 2xl:mx-2 ${customStyles}`}>
+    <Link href={path}>
+      <a href={path} className={`flex  items-center ${alignment}`}>
+        <div className="relative z-10 -mr-8 flex items-center justify-center rounded-xl bg-purple-500 p-4">
+          <Image
+            src="/assets/image/podcasts.svg"
+            alt="talk image"
+            width={32}
+            height={32}
+          />
+        </div>
+        <div className="flex h-fit rounded-3xl bg-gray-800 ">
+          <div className="relative w-1/3 overflow-hidden rounded-3xl">
             <Image
               src="/assets/image/purple.jpg"
               alt="talk image"
               objectFit="cover"
-              width={350}
-              height={350}
-              layout="responsive"
+              layout="fill"
+              width="100%"
+              height="100%"
             />
           </div>
-          <div className="relative -ml-44 h-fit w-2/5 rounded-3xl bg-gray-800 p-10">
-            <h4 className="mb-3 text-2xl font-bold tracking-wide text-gray-300">
+          <div className="h-fit w-2/3 p-5">
+            <h4 className="mb-3 text-ellipsis text-2xl font-bold tracking-wide text-gray-300">
               {title}
             </h4>
-            <p className="mb-5 text-lg tracking-wide text-gray-300">
+            <p className="mb-5 text-ellipsis text-lg tracking-wide text-gray-300">
               {description}
             </p>
             <span className="text-gray-300">{date}</span>
           </div>
-          <div className="z-10 -ml-8 flex items-center justify-center rounded-xl bg-purple-500 p-4">
-            <Image
-              src="/assets/image/podcasts.svg"
-              alt="talk image"
-              width={32}
-              height={32}
-            />
-          </div>
-        </a>
-      </Link>
-    ) : (
-      <Link href={path}>
-        <a href={path} className={`flex items-center ${alignment}`}>
-          <div className="z-20 flex items-center justify-center rounded-xl bg-purple-500 p-4">
-            <Image
-              src="/assets/image/podcasts.svg"
-              alt="talk image"
-              width={32}
-              height={32}
-            />
-          </div>
-          <div className="relative z-10  -ml-8 h-fit  w-2/5 rounded-3xl bg-gray-800 p-10">
-            <h4 className="mb-3 text-2xl font-bold tracking-wide text-gray-300">
-              {title}
-            </h4>
-            <p className="mb-5 text-lg tracking-wide text-gray-300">
-              {description}
-            </p>
-            <span className="text-gray-300">{date}</span>
-          </div>
-          <div className="-ml-44 h-80 w-80 overflow-hidden rounded-3xl">
-            <Image
-              src="/assets/image/purple.jpg"
-              alt="talk image"
-              objectFit="cover"
-              width={350}
-              height={350}
-              layout="responsive"
-            />
-          </div>
-        </a>
-      </Link>
-    )}
+        </div>
+      </a>
+    </Link>
   </li>
 )
 
 PodcastsCard.defaultProps = {
-  mirrored: false,
   customStyles: '',
   alignment: '',
 }
