70b43264d1a823552493028ed72afa7de23f1272
Matheus Corrêa <67432473+kmefeu@users.noreply.github.com>
2022-03-13 16:48:54 -0300

    content: TalksSection

--- src/components/TalksSection/index.tsx
+++ src/components/TalksSection/index.tsx
@@ -6,33 +6,26 @@ const TalksSection: React.FC = () => (
     <h3 className="ml-5 mb-10 bg-gradient-to-br from-cyan-500 to-blue-500 bg-clip-text pb-3 text-2xl font-bold tracking-wide text-transparent sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl  2xl:text-7xl">
       talks
     </h3>
-    <ul className="grid grid-cols-1 gap-10 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
+    <ul className="grid grid-cols-1 gap-10 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3">
       <TalksCard
         title="Dicas de como evoluir profissionalmente como Desenvolvedor Web"
-        description="Bruno começou a carreira como desenvolvedor web em Piracicaba e hoje trabalha na Cisco em Orlando/FL. "
-        path="/talks/1"
-        date="14 novembro 2020"
+        description="Bruno começou a carreira como desenvolvedor web em Piracicaba e hoje trabalha na Cisco em Orlando/FL. Além disso é consultor JavaScript focado em React JS e Organizador do Orlando JS Meetup."
+        path="https://www.youtube.com/watch?v=BmJ1GcAwM3g"
+        date="5 dez 2020"
       />
       <TalksCard
-        title="Dicas de como evoluir profissionalmente como Desenvolvedor Web"
-        description="Bruno começou a carreira como desenvolvedor web em Piracicaba e hoje trabalha na Cisco em Orlando/FL. "
-        path="/talks/1"
-        date="14 novembro 2020"
+        title="Renderizando componentes React no servidor"
+        description="O ReactJS tem a poderosa habilidade de renderizar seus componentes no servidor. Nesta apresentação Bruno irá mostrar os benefícios e também como e quando podemos explorar esta prática."
+        path="https://www.youtube.com/watch?v=sF__9OISv_E"
+        date="5 set 2017"
         customStyles="hidden md:block"
       />
       <TalksCard
-        title="Dicas de como evoluir profissionalmente como Desenvolvedor Web"
+        title="The Future of React With Universal Applications"
         description="Bruno começou a carreira como desenvolvedor web em Piracicaba e hoje trabalha na Cisco em Orlando/FL. "
-        path="/talks/1"
-        date="14 novembro 2020"
+        path="https://www.youtube.com/watch?v=D9a2ovt_CrU"
+        date="9 Fev 2017"
         customStyles="hidden xl:block"
-      />
-      <TalksCard
-        title="Dicas de como evoluir profissionalmente como Desenvolvedor Web"
-        description="Bruno começou a carreira como desenvolvedor web em Piracicaba e hoje trabalha na Cisco em Orlando/FL. "
-        path="/talks/1"
-        date="14 novembro 2020"
-        customStyles="hidden 2xl:block"
       />
     </ul>
     <SeeAll title="talks" path="/talks" />
