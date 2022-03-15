74e3e418c9f9c50fb502afb25bb82e1bf9532470
Matheus Corrêa <67432473+kmefeu@users.noreply.github.com>
2022-03-15 10:13:47 -0300

    fix: unsed variables

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -14,16 +14,16 @@ const About: React.FC = () => (
           <Brand width={180} height={74} />
         </div>
         <p className="text-xl leading-8 tracking-wider text-gray-300 ">
-          Hey, I'm Bruno Genaro. I started developing websites back in 2003,
+          Hey, I&aposm Bruno Genaro. I started developing websites back in 2003,
           working in a music portal called ZineKaos.com.br (RIP), which you
-          still can see its last version created in 2005. I'm currently the CEO
-          of WebSolutionsFL, where we offer consultancy and outsourcing work
+          still can see its last version created in 2005. I&aposm currently the
+          CEO of WebSolutionsFL, where we offer consultancy and outsourcing work
           focused mainly on JavaScript/TypeScript (React, React Native, and
-          Node.js). I'm originally from Brazil, and now I live in Orlando
-          (Florida). When I'm not coding, you can see me at the gym, concerts,
-          skateboarding, playing drums, reading books, taking online courses,
-          producing events, or enjoying the fantastic Florida weather with my
-          beautiful two daughters, Sophie and Stella.
+          Node.js). I&aposm originally from Brazil, and now I live in Orlando
+          (Florida). When I&aposm not coding, you can see me at the gym,
+          concerts, skateboarding, playing drums, reading books, taking online
+          courses, producing events, or enjoying the fantastic Florida weather
+          with my beautiful two daughters, Sophie and Stella.
         </p>
       </div>
     </div>
