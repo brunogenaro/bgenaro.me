aa4d5e15d6e1245d21774dc9f8759bc6f5a0ce83
Matheus Corrêa <67432473+kmefeu@users.noreply.github.com>
2022-04-06 14:44:21 -0300

    feat: brand cursor pointer

--- src/components/Brand/index.tsx
+++ src/components/Brand/index.tsx
@@ -5,22 +5,26 @@ import React from 'react'
 interface iBrand {
   width?: number
   height?: number
+  customClass?: string
 }
 
-const Brand = ({ width, height }: iBrand) => (
-  <Link href="/" passHref>
-    <Image
-      src="/assets/image/brand.svg"
-      alt="Bruno Genaro Brand"
-      width={width}
-      height={height}
-    />
-  </Link>
+const Brand = ({ width, height, customClass }: iBrand) => (
+  <div className={customClass}>
+    <Link href="/" passHref>
+      <Image
+        src="/assets/image/brand.svg"
+        alt="Bruno Genaro Brand"
+        width={width}
+        height={height}
+      />
+    </Link>
+  </div>
 )
 
 Brand.defaultProps = {
   width: 100,
   height: 50,
+  customClass: '',
 }
 
 export default Brand
