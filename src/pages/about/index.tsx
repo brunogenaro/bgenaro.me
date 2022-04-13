aa99b2c68fbd071cca301dd598e737253a53cf5a
Matheus Corrêa <67432473+kmefeu@users.noreply.github.com>
2022-04-13 09:08:11 -0300

    fix: about text

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -7,7 +7,7 @@ import CareerSection from '../../components/CareerSection'
 
 const About: React.FC = () => {
   const text =
-    'Hey, I&#8216;m Bruno Genaro. I started developing websites back in 2003, working in a music portal called ZineKaos.com.br (RIP), which you still can see its last version created in 2005. I&#8216;m currently the CEO of WebSolutionsFL, where we offer consultancy and outsourcing work focused mainly on JavaScript/TypeScript (React, React Native, and Node.js). I&#8216;m originally from Brazil, and now I live in Orlando (Florida). When I&#8216;m not coding, you can see me at the gym, concerts, skateboarding, playing drums, reading books, taking online courses, producing events, or enjoying the fantastic Florida weather with my beautiful two daughters, Sophie and Stella'
+    "Hey, I'm Bruno Genaro. I started developing websites back in 2003, working in a music portal called ZineKaos.com.br (RIP), which you still can see its last version created in 2005. I'm currently the CEO of WebSolutionsFL, where we offer consultancy and outsourcing work focused mainly on JavaScript/TypeScript (React, React Native, and Node.js). I'm originally from Brazil, and now I live in Orlando (Florida). When I'm not coding, you can see me at the gym, concerts, skateboarding, playing drums, reading books, taking online courses, producing events, or enjoying the fantastic Florida weather with my beautiful two daughters, Sophie and Stella"
   return (
     <div className="mt-10 text-gray-300">
       <Toaster />
