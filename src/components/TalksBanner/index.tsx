31288c0a16c3604e9e07116245f985fd1b385ee4
Matheus Corrêa <67432473+kmefeu@users.noreply.github.com>
2022-04-28 20:38:26 -0300

    refactor: TalksBanner mobile

--- src/components/TalksBanner/index.tsx
+++ src/components/TalksBanner/index.tsx
@@ -2,13 +2,13 @@ import React from 'react'
 import ImageShadow from '../ImageShadow'
 
 const TalksBanner: React.FC = () => (
-  <div className="my-10 grid h-96 grid-cols-12 grid-rows-6">
-    <div className="col-span-4 col-start-1 row-span-6 row-start-1 rounded-2xl bg-gray-800" />
-    <div className="col-span-6 col-start-2 row-start-2">
-      <h3 className="mb-2 bg-gradient-to-br from-cyan-500 to-blue-500 bg-clip-text text-9xl font-bold tracking-wide text-transparent">
+  <div className="my-10 grid h-48 grid-cols-12 grid-rows-6 sm:h-60 md:h-60 2xl:h-96">
+    <div className="col-span-3 col-start-1 row-span-6 row-start-1 rounded-2xl bg-gray-800 2xl:col-span-4 2xl:col-start-1" />
+    <div className="col-span-5 col-start-2 row-start-2">
+      <h3 className="mb-2 bg-gradient-to-br from-cyan-500 to-blue-500 bg-clip-text text-2xl font-bold tracking-wide text-transparent md:text-6xl lg:text-7xl xl:text-9xl">
         talks
       </h3>
-      <p className="pl-10 text-3xl leading-9 text-gray-300">
+      <p className="pl-10 text-lg leading-6 text-gray-300 lg:leading-7 xl:text-xl  xl:leading-8 2xl:text-3xl">
         Dicas de como evoluir profissionalmente como Desenvolvedor Web
       </p>
     </div>
