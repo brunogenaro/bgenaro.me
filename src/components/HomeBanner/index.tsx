ae2392f4d16bb534489b813e6c655a14365dc1f8
Matheus Corrêa <67432473+kmefeu@users.noreply.github.com>
2022-05-07 14:21:54 -0300

    feat: HomeBanner redesign

--- src/components/HomeBanner/index.tsx
+++ src/components/HomeBanner/index.tsx
@@ -1,55 +1,60 @@
 import Link from 'next/link'
-import TypeAnimation from 'react-type-animation'
+import { useEffect, useRef, useState } from 'react'
+import * as THREE from 'three'
+import BIRDS from 'vanta/dist/vanta.net.min'
 import Avatar from '../Avatar'
-import InsertionPointer, { ColorAnimation, SpaceHolder } from './styles'
+import { SpaceHolder } from './styles'
 
 const HomeBanner: React.FC = () => {
   const color = 'from-cyan-500 to-blue-500'
   const interval = 3000
 
+  const [vantaEffect, setVantaEffect] = useState(0)
+  const vantaRef = useRef(null)
+
+  useEffect(() => {
+    if (!vantaEffect) {
+      setVantaEffect(
+        BIRDS({
+          el: vantaRef.current,
+          THREE,
+          mouseControls: true,
+          touchControls: false,
+          gyroControls: false,
+          minHeight: 200.0,
+          minWidth: 200.0,
+          scale: 1.0,
+          scaleMobile: 1.0,
+          color: 0x3b82f6,
+          backgroundColor: 0x111827,
+          showDots: true,
+          points: 20.0,
+          maxDistance: 20.0,
+          spacing: 20.0,
+        }),
+      )
+    }
+    return () => {
+      if (vantaEffect) (vantaEffect as any).destroy()
+    }
+  }, [vantaEffect])
+
   return (
     <Link href="/about" passHref>
-      <section className="relative my-32 flex items-center justify-between 2xl:px-20">
+      <section
+        className="relative my-10 flex items-center justify-between overflow-hidden rounded-3xl p-20"
+        ref={vantaRef}
+      >
         <div className="cursor-pointer">
-          <span className="ml-1 text-sm tracking-wide text-gray-300 sm:text-base md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl">
+          <span className=" text-sm tracking-wide text-gray-300 sm:text-base md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl">
             Hello, I&#8216;m
           </span>
-          <h1 className="text-3xl font-bold tracking-wide text-gray-300 sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl 2xl:text-8xl">
+          <h1 className="bg-gradient-to-br from-cyan-500 to-blue-500 bg-clip-text text-3xl font-bold tracking-wide text-transparent sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl 2xl:text-8xl">
             Bruno Genaro
           </h1>
-          <ColorAnimation className="flex items-center">
-            <TypeAnimation
-              cursor
-              wrapper="h2"
-              repeat={Infinity}
-              sequence={[
-                'Senior Software Engineer',
-                interval,
-                'JavaScript Consultant',
-                interval,
-                'CEO at WebSolutionsFL',
-                interval,
-                'Speaker',
-                interval,
-                'Event Producer',
-                interval,
-                'OrlandoJS Meetup Co-Organizer',
-                interval,
-                'Father to two beautiful daughters',
-                100,
-                'beautiful daughters',
-                interval,
-              ]}
-              className={`bg-gradient-to-br ${color} bg-clip-text pb-3 text-3xl font-bold tracking-wide text-transparent sm:text-4xl md:text-5xl lg:text-6xl`}
-            />
-
-            <InsertionPointer className={`bg-gradient-to-br ${color}`} />
-          </ColorAnimation>
         </div>
         <SpaceHolder />
-        <div className="cursor-pointer">
-          <Avatar />
-        </div>
+        <Avatar />
       </section>
     </Link>
   )
