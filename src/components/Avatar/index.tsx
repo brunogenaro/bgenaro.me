471edf3a3b22275cfa94f41ce8916994b83e0648
Matheus Corrêa <67432473+kmefeu@users.noreply.github.com>
2022-05-07 18:02:16 -0300

    feat: Avatar

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -1,26 +1,21 @@
 import Image from 'next/image'
 import React from 'react'
-import ImageContainer, { AvatarPosition, ImageBorder } from './styles'
+import ImageContainer from './styles'
 
 interface iAvatar {
   src?: string
   alt?: string
 }
 const Avatar = ({ src, alt }: iAvatar) => (
-  <AvatarPosition>
-    <div className="relative">
-      <ImageBorder className="absolute flex items-center justify-center rounded-full bg-gradient-to-br from-cyan-500 to-blue-500 shadow-lg shadow-cyan-500/50" />
-      <ImageContainer className="absolute overflow-hidden rounded-full">
-        <Image
-          src={src || ''}
-          alt={alt}
-          width={350}
-          height={350}
-          objectFit="cover"
-        />
-      </ImageContainer>
-    </div>
-  </AvatarPosition>
+  <ImageContainer className="overflow-hidden rounded-2xl">
+    <Image
+      src={src || ''}
+      alt={alt}
+      width={350}
+      height={350}
+      objectFit="cover"
+    />
+  </ImageContainer>
 )
 
 Avatar.defaultProps = {
