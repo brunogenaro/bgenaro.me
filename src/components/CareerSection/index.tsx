52696af4cb5eedb8770b702ba0dbad143f2daea5
Matheus Corrêa <67432473+kmefeu@users.noreply.github.com>
2022-05-11 12:14:11 -0300

    feat: career dropdown

--- src/components/CareerSection/index.tsx
+++ src/components/CareerSection/index.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import React, { useState } from 'react'
 
 export interface iCareer {
@@ -18,13 +19,13 @@ const CareerSection: React.FC<iCareerSection> = ({
   career,
 }: iCareerSection) => {
   const [activeItem, setActiveItem] = useState(career[0])
-
+  const [showItem, setShowItem] = useState(false)
   const isActive = (item: iCareer) =>
     activeItem.company.name === item.company.name
 
   return (
     <section className="mt-10 grid grid-cols-12 text-xl leading-8 tracking-wider">
-      <ul className=" col-start-1 col-span-2 lg:col-span-2 lg:col-start-3 mt-16 text-right">
+      <ul className="hidden md:block col-start-2 col-span-2 lg:col-span-2 lg:col-start-3 mt-16 text-right">
         {career.map(item => (
           <li className="flex justify-end" key={item.company.name}>
             <button
@@ -36,16 +37,66 @@ const CareerSection: React.FC<iCareerSection> = ({
                 isActive(item) && 'border-blue-500 text-blue-500 '
               }`}
             >
-              <span className="transition-all delay-200 ease-in-out hover:text-2xl  hover:text-blue-500">
+              <span className="transition-all delay-200 ease-in-out hover:text-2xl  hover:text-blue-500 whitespace-nowrap">
                 {item.company.name}
               </span>
             </button>
           </li>
         ))}
       </ul>
-      <div className="col-span-9 col-start-4 lg:col-span-6 lg:col-start-6">
+      <div className="block md:hidden">
+        <div className="flex items-baseline">
+          <h3 className="text-3xl font-bold">Career</h3>
+          <span className="ml-2 text-sm whitespace-nowrap ">
+            (select to see other companies)
+          </span>
+        </div>
+        <div className="my-3">
+          <button
+            onClick={() => {
+              setShowItem(!showItem)
+            }}
+            type="button"
+            className="flex rounded-md tracking-wider p-2 align-middle text-lg font-bold text-blue-500 duration-200 ease-in-out hover:scale-105 whitespace-nowrap"
+          >
+            {activeItem.company.name}
+            <div className="ml-3 flex self-center">
+              <Image
+                src={
+                  showItem
+                    ? '/assets/image/svg/action/up.svg'
+                    : '/assets/image/svg/action/down.svg'
+                }
+                width={24}
+                height={24}
+                layout="fixed"
+              />
+            </div>
+          </button>
+        </div>
+        {showItem && (
+          <div className="relative">
+            <ul className="absolute mt-2 z-10 bg-white divide-y divide-gray-100 rounded shadow dark:bg-gray-700 dark:divide-gray-600">
+              {career.map(item => (
+                <li key={item.company.name}>
+                  <button
+                    onClick={() => {
+                      setActiveItem(item)
+                    }}
+                    type="button"
+                    className="px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white w-full text-left"
+                  >
+                    {item.company.name}
+                  </button>
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
+      <div className="col-span-10 col-start-1 md:col-span-9 md:col-start-5 lg:col-span-6 lg:col-start-6">
         <header>
-          <h3 className="mb-5 text-3xl font-bold">Career</h3>
+          <h3 className="hidden md:block mb-5 text-3xl font-bold">Career</h3>
           <h4>
             <span className="mr-3 font-bold">{activeItem.title}</span>
             <a
