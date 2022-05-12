8c501d92bc4c7c1ccc92c981efa9ceabf8168d42
Matheus Corrêa <67432473+kmefeu@users.noreply.github.com>
2022-05-12 14:25:06 -0300

    feat: sendgrid env config

--- src/pages/api/sendgrid.ts
+++ src/pages/api/sendgrid.ts
@@ -6,11 +6,13 @@ sendgrid.setApiKey(process.env.SENDGRID_API_KEY || '')
 async function sendEmail(req: NextApiRequest, res: NextApiResponse) {
   try {
     await sendgrid.send({
-      to: 'sowopa9450@akapple.com', // Change to your recipient
-      from: 'mcorreadesign@gmail.com', // Change to your verified sender
+      to: process.env.SENDGRID_EMAIL_TO || '', // Change to your recipient
+      from: process.env.SENDGRID_EMAIL_FROM || '', // Change to your verified sender
       subject: req.body.subject,
       text: req.body.message,
-      html: `<a href='www.google.com'>${req.body.message}</a>`,
+      html: `<span>${req.body.message}</span>
+      <br>
+      <b>${req.body.name}</b>`,
     })
     return res.status(200).send('Email sent successfully')
   } catch (error: unknown) {
