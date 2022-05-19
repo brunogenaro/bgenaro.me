a1729a63dbc3fd4b8b2dc03b811fb54a239c5781
Matheus Corrêa <67432473+kmefeu@users.noreply.github.com>
2022-05-19 19:10:23 -0300

    feat: avatar image priority prop

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -20,6 +20,7 @@ const Avatar = ({ src, alt, gradient }: iAvatar) => (
         width="100%"
         height="100%"
         layout="responsive"
+        priority
       />
     </div>
   </GradientBackground>
